b768b7e16da9f52133e06379571968e0865fed0d
Uwe L. Korn <uwelk@xhochy.com>
2015-04-04 18:45:46 +0200

    Add resolve command

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+
 // Parse the commandline arguments
 var args = process.argv.slice(2);
 
@@ -17,6 +19,34 @@ var commands = {
         }
         console.error("Not yet implemented. Please stand by!");
         // FIXME: Implement!
+    },
+    "resolve": function (args) {
+        if (args.length == 0) {
+            console.error("You need to specify a resolver to utilise.");
+            process.exit(1);
+        }
+        if (args.length < 3 || args.length > 4) {
+            console.error("Usage: \n\t tomahawkjs resolve <resolver> <artist> <title> [<album>]");
+            process.exit(1);
+        }
+        fs.stat(args[0], function (err, stats) {
+            if (err) {
+                console.error("Error while reading the resolver path:");
+                console.error(err);
+                process.exit(1);
+            }
+            if (stats.isFile()) {
+                // Load the resolver from a (zipped) bundle.
+                // FIXME: Implement!
+            } else if (stats.isDirectory()) {
+                // Load the resolver from a directory.
+                // FIXME: Implement!
+            } else {
+                // Will be interesting what kind of fs type people will access here
+                console.error("Unsupported FS item for a resolver bundle.");
+                process.exit(1);
+            }
+        });
     }
 }
 
