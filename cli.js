fced2910c7289fc3afcaf2da3acdc70f44e17f1c
Uwe L. Korn <uwelk@xhochy.com>
2015-04-04 19:27:04 +0200

    Add failOn helper for cleaner exit code

--- cli.js
+++ cli.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 
 var commands = require('./lib/cli/commands.js');
+var failOn = require('./lib/cli/utils.js').failOn;
 
 // Parse the commandline arguments
 var args = process.argv.slice(2);
 
 // The first argument is the command.
-if (args.length == 0) {
-    console.error("You need to specify the command that shall be executed.")
-    process.exit(1);
-}
+failOn(args.length == 0, "You need to specify the command that shall be executed.");
 
-if (commands.hasOwnProperty(args[0])) {
-    commands[args[0]](args.slice(1));
-} else {
-    console.error("The specified command is unknown.");
-    process.exit(1);
-}
+// Check if we have a matching command defined.
+failOn(!commands.hasOwnProperty(args[0]), "The specified command is unknown.");
+
+// Execute the command.
+commands[args[0]](args.slice(1));
